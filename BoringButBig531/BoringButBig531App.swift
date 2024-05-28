6dca434754e47a61b2c706a7af3610624614894f
CharlesDeda <dedaplays@yahoo.com>
2024-05-28 13:12:25 -0400

    Initial Commit

---
+++ BoringButBig531/BoringButBig531App.swift
@@ -0,0 +1,17 @@
+//
+//  BoringButBig531App.swift
+//  BoringButBig531
+//
+//  Created by Nick Deda on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BoringButBig531App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
