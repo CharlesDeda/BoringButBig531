ece1553eca0acdbd7b7838c0a67654e3741115f7
CharlesDeda <dedaplays@yahoo.com>
2024-06-06 08:22:05 -0400

    o

--- BoringButBig531/PlanView.swift
+++ BoringButBig531/PlanView.swift
@@ -1,16 +1,5 @@
 import SwiftUI
 
-/*
- lifts needs to toggle each set individually
- forEach Binding
- plan should not be a double
- */
-
-//struct Sets {
-//  var sets = [0,0,0,0,0]
-//  var isComplete = false
-//}
-
 struct PlanView: View {
   @Environment(\.dismiss) var dismiss
   @State var lift: Lift
@@ -25,26 +14,22 @@ struct PlanView: View {
   }
   
   var body: some View {
-    VStack {
-      List {
-        Section("5/3/1") {
-          ForEach(plan.weights.indices) { index in
-            HStack {
-              Text(plan.weights[index], format: .number)
-              Text("\(plan.reps[index])")
-                .frame(maxWidth: .infinity, alignment: .trailing)
-            }
+    List {
+      Section("5/3/1") {
+        ForEach(plan.weights.indices) { index in
+          HStack {
+            Text(plan.weights[index], format: .number)
+            Text("\(plan.reps[index])")
+              .frame(maxWidth: .infinity, alignment: .trailing)
           }
         }
       }
-      List {
-        Section("Boring But Big") {
-          ForEach(bbbPlan.weights.indices) { index in
-            HStack {
-              Text(bbbPlan.weights[index], format: .number)
-              Text("\(bbbPlan.reps[index])")
-                .frame(maxWidth: .infinity, alignment: .trailing)
-            }
+      Section("Boring But Big") {
+        ForEach(bbbPlan.weights.indices) { index in
+          HStack {
+            Text(bbbPlan.weights[index], format: .number)
+            Text("\(bbbPlan.reps[index])")
+              .frame(maxWidth: .infinity, alignment: .trailing)
           }
         }
       }
